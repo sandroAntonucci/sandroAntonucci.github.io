51da266cf714f007fb3d1ccbd2ebde559ecd3b94
Devream <devream24@gmail.com>
2024-12-23 11:41:39 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const header = document.querySelector("header");
 
 window.addEventListener ("scroll", function(){
 
-    header.classList.toggle ("sticky", window.scrollY > 100);
+    header.classList.toggle ("sticky", window.scrollY > 120);
 
 });
 
